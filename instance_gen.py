90430023e09c3ed38a0f2cb5e8816be5b282c4de
Elias <elias@baez.us>
2021-03-09 20:32:08 -0500

    Trying to fix keypair issue

--- instance_gen.py
+++ instance_gen.py
@@ -4,7 +4,7 @@ ec2 = boto3.resource('ec2')
 instances = ec2.create_instances(
   ImageId='ami-09246ddb00c7c4fef',
   MinCount=1,
-  MaxCount=2,
+  MaxCount=1,
   InstanceType='t2.micro',
   KeyName='ec2-keypair'
 )
