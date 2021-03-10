64ef4c2105def75ed9d78ea1a42b2736c40932cf
Elias <elias@baez.us>
2021-03-09 20:18:54 -0500

    Updated with git ignore

--- instance_gen.py
+++ instance_gen.py
@@ -2,7 +2,7 @@ import boto3
 ec2 = boto3.resource('ec2')
 
 instances = ec2.create_instances(
-  ImageId='ami-2f442839',
+  ImageId='ami-09246ddb00c7c4fef',
   MinCount=1,
   MaxCount=2,
   InstanceType='t2.micro',
